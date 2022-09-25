f8b7f5e83062605e286d05363967bbbbe527e329
dw1209 <daniel20001209@gmail.com>
2022-09-26 00:54:31 +0800

    Add database

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,27 @@
-from flask import Blueprint, render_template, request, flash
+from . import db
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User.query.filter_by(username=username).first()
+
+        if not user:
+            flash('Username does not exist.', category='error')
+        else:
+            if not check_password_hash(user.password, password):
+                flash('Incorrect password, try again.', category='error')
+            else:
+                flash('Login successfully!', category='success')
+                return redirect(url_for('views.home'))
+
     return render_template('login.html')
 
 @auth.route('/register', methods=['GET', 'POST'])
@@ -13,15 +30,22 @@ def register():
         email = request.form.get('email')
         username = request.form.get('username')
         password = request.form.get('password')
+        user = User.query.filter_by(username=username).first()
 
-        if len(email) < 3:
+        if user:
+            flash('Username has already existed.', category='error')
+        elif len(email) < 3:
             flash('Email is invalid.', category='error')
         elif len(username) < 2:
             flash('Username must be greater than 1 character.', category='error')
         elif len(password) < 3:
             flash('Password must be at least 3 characters.', category='error')
         else:
+            new_user = User(email=email, username=username, password=generate_password_hash(password, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account created!', category='success')
+            return redirect(url_for('auth.login'))
 
     return render_template('register.html')
 
