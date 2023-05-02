e520503ed8f3c1fe594ad638fc4c250b36c74911
dw1209 <daniel20001209@gmail.com>
2023-05-02 17:40:06 +0800

    change variable from global to local and disable warning message

--- website/__init__.py
+++ website/__init__.py
@@ -10,6 +10,7 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'f8802a6cf36244f3b35a1f2700c24f86'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
     db.init_app(app)
     create_database(app)
